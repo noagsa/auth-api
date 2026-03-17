25ee0ca00ff0445b9c3a9f2b383871407279ad46
Noa <noasalgadodev@gmail.com>
2026-03-17 17:54:06 +0100

    feat: add auth service and connect to controller

--- src/main/java/io/github/noagsa/authapi/controller/AuthController.java
+++ src/main/java/io/github/noagsa/authapi/controller/AuthController.java
@@ -2,6 +2,7 @@ package io.github.noagsa.authapi.controller;
 
 import io.github.noagsa.authapi.dto.AuthRequestDTO;
 import io.github.noagsa.authapi.dto.AuthResponseDTO;
+import io.github.noagsa.authapi.service.AuthService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,13 +11,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("register")
     public AuthResponseDTO registerUser(@RequestBody AuthRequestDTO requestDTO) {
-        return mull;
+
+        return authService.register(requestDTO);
     }
 
     @PostMapping("login")
     public AuthResponseDTO loginUser(@RequestBody AuthRequestDTO requestDTO) {
-        return null;
+
+        return authService.login(requestDTO);
     }
 }
