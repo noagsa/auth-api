896799a014e3905b3930113435d7218e569b9d14
Noa <noasalgadodev@gmail.com>
2026-03-19 22:08:52 +0100

    refactor: rename TestController to DemoController and update routes

--- src/main/java/io/github/noagsa/authapi/controller/DemoController.java
+++ src/main/java/io/github/noagsa/authapi/controller/DemoController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/test")
-public class TestController {
+@RequestMapping("api/demo")
+public class DemoController {
     @GetMapping
     public String getTestMessage() {
         return "Access granted";
